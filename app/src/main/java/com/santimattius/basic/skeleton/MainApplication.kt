ef8637a5a0246b96a38bcc3897c3c8771792335b
Santiago Mattiauda <santimattius@gmail.com>
2025-01-27 20:46:40 -0300

    [update] update gradle and koin implementation

--- app/src/main/java/com/santimattius/basic/skeleton/MainApplication.kt
+++ app/src/main/java/com/santimattius/basic/skeleton/MainApplication.kt
@@ -5,14 +5,14 @@ import com.santimattius.basic.skeleton.di.AppModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.androix.startup.KoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
-import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.KoinConfiguration
 import org.koin.ksp.generated.defaultModule
 import org.koin.ksp.generated.module
 
 @OptIn(KoinExperimentalAPI::class)
 class MainApplication : Application(), KoinStartup {
 
-    override fun onKoinStartup(): KoinAppDeclaration = {
+    override fun onKoinStartup(): KoinConfiguration = KoinConfiguration {
         androidContext(this@MainApplication)
         allowOverride(false)
         modules(AppModule().module)
